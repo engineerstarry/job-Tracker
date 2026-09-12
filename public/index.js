1b3e362c67b7d773e250e3081413487edb58bd6a
test <test@119-iMac-215-3069-2017-2021--3GHZ-25616-T3.local>
2026-09-12 15:00:37 +0100

    i designed the hamburger menu and gave it a great ui

--- public/index.js
+++ public/index.js
@@ -1,3 +1,8 @@
 $("#menu-btn").click(function() {
     $("#menu").toggleClass("show");
+    if ($(this).text() === "☰") {
+        $(this).text("X");
+    } else {
+        $(this).text("☰");
+    }
 })

28a99307ecede008a27c907fab5c39044466cf78
test <test@119-iMac-215-3069-2017-2021--3GHZ-25616-T3.local>
2026-09-13 16:56:33 +0100

    still giving styling to the css of the home page

--- public/index.js
+++ public/index.js
@@ -6,3 +6,18 @@ $("#menu-btn").click(function() {
         $(this).text("☰");
     }
 })
+
+
+$(".status").each(function () {
+
+    const jobStatus = $(this).text().trim();
+
+    if (jobStatus === "Applied") {
+        $(this).addClass("applied");
+    } else if (jobStatus === "Interviewing") {
+        $(this).addClass("interview");
+    } else if (jobStatus === "Offer Received") {
+        $(this).addClass("accepted");
+    }
+
+});
